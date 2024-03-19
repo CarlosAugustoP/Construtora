fabf28b9c44bf8b19851ed558bd76c3818571e8d
Carlos Augusto <capv2004@gmail.com>
2024-03-19 16:42:03 -0300

    progress

--- src/components/QuemSomos/index.js
+++ src/components/QuemSomos/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Container, ImageContainer, TextContainer, TitleContainer, Text } from "./styles";
-import photo1 from '../../../public/img/QuemSomosPhoto1.jpeg';
+import photo1 from '../../../public/img/QuemSomosPhoto1.jpg';
 const lorem ="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut ultricies mi. Sed erat lorem, porttitor eu mauris vitae, condimentum vulputate sem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis pretium ipsum nec dui fringilla, quis interdum tellus tristique. Mauris porttitor sapien quam, s. Cras et purus purus "
 
-const ContentSection = ({ imgSrc, titleText, bodyText, direction = "row" }) => {
-    return (
-      <Container style = {{flexDirection : direction}}>
+const ContentSection = ({ imgSrc, titleText, bodyText, direction = "row", backgroundColor= "rgb(18,18,18,0.8)", color = "white"}) => {
+  return (
+      <Container direction = {direction}  style={{ backgroundColor: backgroundColor, color: color }}>
         <TextContainer>
           <h1>{titleText}</h1>
           <Text>{bodyText}</Text>
@@ -16,8 +16,7 @@ const ContentSection = ({ imgSrc, titleText, bodyText, direction = "row" }) => {
       </Container>
     );
   };  
-  
-  export default function QuemSomos() {
+    export default function QuemSomos() {
     return (
         <>
       <div style={{
@@ -34,7 +33,7 @@ const ContentSection = ({ imgSrc, titleText, bodyText, direction = "row" }) => {
         alignItems: 'center', 
         marginTop: '40px'
       }}>
-        <ContentSection imgSrc={photo1} titleText="Compromisso" bodyText={lorem} direction = "row-reverse" />
+        <ContentSection imgSrc={photo1} direction = "row-reverse" titleText="Compromisso" bodyText={lorem} backgroundColor="rgb(238, 238, 238, 0.8)" color= "black"/>
       </div>
        <div style={{
         display: 'flex',
--- src/components/QuemSomos/styles.js
+++ src/components/QuemSomos/styles.js
@@ -3,35 +3,66 @@ import React from 'react';
 export const Container = styled.div`
     width: 95%;
     display: flex;
-    gap:20px;
     border-radius: 25px;
     border: 1px solid white;
-    `;
- export const ImageContainer = styled.div`
-    width: 40%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    height: 400px;
-    padding-right: 20px;
+    flex-direction: ${props => props.direction || 'row-reverse'};
 
-    img {
-        height:90%;
-        width:90%;
-        border-radius: 25px;
+    @media (max-width: 768px) {
+        flex-direction: column-reverse;
+        justify-content: center;
+        align-items: center;
+        padding:10px;
+
     }
+`;
+ export const ImageContainer = styled.div`
+ width: 50%;
+ display: flex;
+ justify-content: center;
+ align-items: center;
+ flex-direction: column;
+ height: 400px;
+
+ img {
+     object-fit: cover;
+     height: 90%;
+     width: 90%;
+ }
+
+ @media (max-width: 768px) {
+     width: 100%; /* Take full width on small screens */
+     height: auto; /* Adjust height automatically */
+     
+     img {
+         width: 80%; /* Adjust image width on small screens */
+         height: auto; /* Adjust image height automatically */
+     }
+ }
 `;
 
 export const TextContainer = styled.div`
-    width: 60%;
-    padding-left: 20px;
+    width: 50%;
+    padding: 20px;
+
+    @media (max-width: 768px) {
+         /* Remove padding on small screens */
+            padding: 0;
+            width: 90%;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+    }
 
 `;
 
-export const TitleContainer = styled.div`
+export const H1 = styled.h1`
     width:100%;
     height:20%;
+
+    @media (max-width: 768px) {
+        padding:10px;
+
+    }
 `;
 
 export const Text = styled.p`
